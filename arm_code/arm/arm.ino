d68dad46d2bd62fda338a1028709097525ea9116
richardvo <richardvo@gmail.com>
2022-01-27 23:24:35 +0100

    init

--- arm_code/arm/arm.ino
+++ arm_code/arm/arm.ino
@@ -1,20 +1,11 @@
 /*
-  Arduino Starter Kit example
-  Project 5 - Servo Mood Indicator
-
-  This sketch is written to accompany Project 5 in the Arduino Starter Kit
 
   Parts required:
   - servo motor
   - 10 kilohm potentiometer
   - two 100 uF electrolytic capacitors
 
-  created 13 Sep 2012
-  by Scott Fitzgerald
 
-  https://store.arduino.cc/genuino-starter-kit
-
-  This example code is part of the public domain.
 */
 
 // include the Servo library
@@ -46,7 +37,6 @@ void loop() {
 
   // set the servo position
   myServo.write(angle);
-
   // wait for the servo to get there
   delay(15);
 }

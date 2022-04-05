5f65f8e68979d903d765fbf1f3cf74c981bcb8a6
richardvo <richardvo@gmail.com>
2022-04-05 18:08:02 +0200

    limit value + log

--- arm_code/arm_v2/arm_v2.ino
+++ arm_code/arm_v2/arm_v2.ino
@@ -5,23 +5,15 @@ Servo servoArm1;
 Servo servoArm2;
 Servo servoClaw;
 
-const int buttonClosePin = 12;
-const int buttonOpenPin = 13; 
-int buttonCloseState = 0; 
-int buttonOpenState = 0; 
-
-int const potPinServoRot = A0; // analog pin used to connect the potentiometer
-int const potPinServoArm1 = A1; // analog pin used to connect the potentiometer
-int const potPinServoArm2 = A2; // analog pin used to connect the potentiometer
-
-int potValServoRot;  // variable to read the value from the analog pin
-int potValServoArm1;  // variable to read the value from the analog pin
-int potValServoArm2;  // variable to read the value from the analog pin
-
 int angleServoRot;   // variable to hold the angle for the servo motor
 int angleServoArm1;   // variable to hold the angle for the servo motor
 int angleServoArm2;   // variable to hold the angle for the servo motor
 int angleServoClaw;   // variable to hold the angle for the servo motor
+
+int angleServoRotMap;   // variable to hold the angle for the servo motor
+int angleServoArm1Map;   // variable to hold the angle for the servo motor
+int angleServoArm2Map;   // variable to hold the angle for the servo motor
+int angleServoClawMap;   // variable to hold the angle for the servo motor
 
 
 void setup() {
@@ -29,10 +21,7 @@ void setup() {
   servoArm1.attach(9);
   servoArm2.attach(10);
   servoClaw.attach(11);
-  pinMode(buttonClosePin, INPUT);
-  pinMode(buttonOpenPin, INPUT);
-  digitalWrite(buttonClosePin, HIGH);
-  digitalWrite(buttonOpenPin, HIGH);
+
   Serial.begin(9600); // open a serial connection to your computer
 
 // init ou pas du bras au milieu
@@ -40,8 +29,6 @@ void setup() {
   angleServoArm1 = 50;
   angleServoArm2 = 50;
   angleServoClaw = 50;
-
-
   
 }
 
@@ -61,39 +48,54 @@ while (Serial.available() > 0) {
 
     // look for the newline. That's the end of your sentence:
     if (Serial.read() == '\n') {
-      // constrain the values to 0 - 255 and invert
-      // if you're using a common-cathode LED, just use "constrain(color, 0, 255);"
-
 
      angleServoRot = angleServoRot + cmdAngleServoRot;
+     angleServoRot = min(angleServoRot, 100);
+     angleServoRot = max(angleServoRot, 0);
+     Serial.print("\n angleServoRot: ");
+     Serial.println(angleServoRot);
+          
      angleServoArm1 = angleServoArm1 + cmdAngleServoArm1;
+     angleServoArm1 = min(angleServoArm1, 100);
+     angleServoArm1 = max(angleServoArm1, 0);
+     Serial.print("\n angleServoArm1: ");
+     Serial.println(angleServoArm1);
+      
      angleServoArm2 = angleServoArm2 + cmdAngleServoArm2;
+     angleServoArm2 = min(angleServoArm2, 100);
+     angleServoArm1 = max(angleServoArm2, 0);
+     Serial.print("\n angleServoArm2: ");
+     Serial.println(angleServoArm2);
+      
      angleServoClaw = angleServoClaw + cmdAngleServoClaw;
-
+     angleServoClaw = min(angleServoClaw, 100);
+     angleServoClaw = max(angleServoClaw, 0);
+     Serial.print("\n angleServoClaw: ");
+     Serial.println(angleServoClaw); 
 
     }
   }
 
 
   // scale the numbers from the pot & print out the angle for the servo motor
-  angleServoRot = map(angleServoRot, 0, 100, 60, 120);
-  Serial.print(", angleServoRot: ");
-  Serial.println(angleServoRot);
-  angleServoArm1 = map(angleServoArm1, 0, 100, 63, 179);
-  Serial.print(", angleServoArm1: ");
-  Serial.println(angleServoArm1);
-  angleServoArm2 = map(angleServoArm2, 0, 100, 47, 126);
-  Serial.print(", angleServoArm2: ");
-  Serial.println(angleServoArm2);
-  angleServoClaw = map(angleServoClaw, 0, 100, 1, 179);
-  Serial.print(", angleServoClaw: ");
-  Serial.println(angleServoClaw);
+  angleServoRotMap = map(angleServoRot, 0, 100, 60, 120);
+  Serial.print(", angleServoRotMap: ");
+  Serial.println(angleServoRotMap);
+  angleServoArm1Map = map(angleServoArm1, 0, 100, 63, 179);
+  Serial.print(", angleServoArm1Map: ");
+  Serial.println(angleServoArm1Map);
+  angleServoArm2Map = map(angleServoArm2, 0, 100, 47, 126);
+  Serial.print(", angleServoArm2Map: ");
+  Serial.println(angleServoArm2Map);
+  angleServoClawMap = map(angleServoClaw, 0, 100, 1, 179);
+  Serial.print(", angleServoClawMap: ");
+  Serial.println(angleServoClawMap);
 
   // set the servo position
-  servoRot.write(angleServoRot);
-  servoArm1.write(angleServoArm1);
-  servoArm2.write(angleServoArm2);
-  servoClaw.write(angleServoClaw);
+  servoRot.write(angleServoRotMap);
+  servoArm1.write(angleServoArm1Map);
+  servoArm2.write(angleServoArm2Map);
+  servoClaw.write(angleServoClawMap);
   
   // wait for the servo to get there
   delay(30);

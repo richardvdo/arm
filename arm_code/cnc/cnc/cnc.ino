0380790626c292df3da5d42a30978bb79dc5975f
richardvdo <richardvdo@gmail.com>
2024-04-18 19:04:52 +0200

    first commit

--- arm_code/cnc/cnc/cnc.ino
+++ arm_code/cnc/cnc/cnc.ino
@@ -19,16 +19,45 @@
 //
 //           - correlé arm
 //
+
+ /* LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ * LCD VSS pin to ground
+ * LCD VCC pin to 5V
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3) */
+ //  http://www.arduino.cc/en/Tutorial/LiquidCrystalHelloWorld
 //##################################################################################
 
-
+#include <LiquidCrystal.h>
 #include <Servo.h>
-#include <Stepper.h>
+#include "BasicStepperDriver.h"
 
 
 
-const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
+#define MOTOR_STEPS 200
+#define RPM 120  // change this to fit the number of steps per revolution
 // for your motor
+
+// Since microstepping is set externally, make sure this matches the selected mode
+// If it doesn't, the motor will move at a different RPM than chosen
+// 1=full step, 2=half step etc.
+#define MICROSTEPS 1
+
+// All the wires needed for full functionality
+#define DIR 8
+#define STEP 9
+//Uncomment line to use enable/disable functionality
+//#define SLEEP 13
+
+// 2-wire basic config, microstepping is hardwired on the driver
+BasicStepperDriver stepper(MOTOR_STEPS, DIR, STEP);
 
 const int buttonUpPin = 16;
 const int buttonDownPin = 17; 
@@ -42,7 +71,7 @@ int const potPinDistance = A4;
 int const potPinLiquideQt = A5;
 
 // initialize the stepper library on pins 8 through 11:
-Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+
 
 Servo servoRot;  // create a servo object
 Servo servoArm1;
@@ -68,12 +97,19 @@ int angleServoArm2;   // variable to hold the angle for the servo motor
 int angleServoClaw;   // variable to hold the angle for the servo motor
 
 
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+
+
 void setup() {
   // set the speed at 60 rpm:
-  myStepper.setSpeed(60);
+  stepper.begin(RPM, MICROSTEPS);
 
 
-
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("hello, world!");
 
 
 
@@ -90,15 +126,27 @@ void setup() {
 
 void loop() {
 // block perceuse
-  // step one revolution  in one direction:
-  Serial.println("clockwise");
-  myStepper.step(stepsPerRevolution);
-  delay(500);
+    // energize coils - the motor will hold position
+    // stepper.enable();
+  
+    /*
+     * Moving motor one full revolution using the degree notation
+     */
+    stepper.rotate(360);
 
-  // step one revolution in the other direction:
-  Serial.println("counterclockwise");
-  myStepper.step(-stepsPerRevolution);
-  delay(500);
+    /*
+     * Moving motor to original position using steps
+     */
+    stepper.move(-MOTOR_STEPS*MICROSTEPS);
+
+    // pause and allow the motor to be moved by hand
+    // stepper.disable();
+
+    delay(5000);
+
+// block screen
+
+
 
 
 // partie arm
